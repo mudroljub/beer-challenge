5bec9374a47e83361a63656b5e94d4f2f793934a
mudroljub <mudroljub@gmail.com>
2025-01-16 14:54:37 +0100

    minor

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -15,40 +15,40 @@ type Item = {
 /* INIT */
 
 async function fetchData() {
-  const response = await fetch('https://api.jsonbin.io/v3/b/6630fd9be41b4d34e4ecd1f9')
-  const data = await response.json()
+  const response = await fetch('https://api.jsonbin.io/v3/b/6630fd9be41b4d34e4ecd1f9');
+  const data = await response.json();
 
-  const container = document.getElementById('container')!
+  const container = document.getElementById('container')!;
   
   data.record.forEach((item: Item) => {
-    const div = document.createElement('div')
-    div.className = `item bg${Math.min(Math.floor(item.ibu / 10), 9)}`
+    const div = document.createElement('div');
+    div.className = `item bg${Math.min(Math.floor(item.ibu / 10), 9)}`;
     div.innerHTML = `
       <p class="ibu ribbon">IBU: ${item.ibu}</p>
       <p class="abv">ABV: ${item.abv}</p>
       <img src="${item.image_url}" />
       <h3>${item.name}</h3>
-    `
-    div.onclick = () => showModal(item)
-    container.appendChild(div)
-  })
+    `;
+    div.onclick = () => showModal(item);
+    container.appendChild(div);
+  });
 }
 
-fetchData()
+fetchData();
 
 /* EVENTS */
 
 function showModal(item: Item) {
-  modal.hidden = false
-  document.getElementById('modal-description')!.textContent = item.description
+  modal.hidden = false;
+  document.getElementById('modal-description')!.textContent = item.description;
   const modalImage = document.getElementById('modal-image') as HTMLImageElement;
-  modalImage.src = item.image_url;  
+  modalImage.src = item.image_url;
 }
 
 closeButton.addEventListener('click', () => {
-  modal.hidden = orderMenu.hidden = true
-})
+  modal.hidden = orderMenu.hidden = true;
+});
 
 orderBtn.addEventListener('click', () => {
   orderMenu.hidden = !orderMenu.hidden;
-})
+});
